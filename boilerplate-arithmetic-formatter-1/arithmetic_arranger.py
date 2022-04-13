755594e08d7e2fbb87c1cfc2fe693cc300432c34
RedOne <elkaboussi@pm.me>
2022-04-13 11:55:38 +0000

    [Solved]

--- boilerplate-arithmetic-formatter-1/arithmetic_arranger.py
+++ boilerplate-arithmetic-formatter-1/arithmetic_arranger.py
@@ -1,52 +1,79 @@
-'''
-["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]
+def arithmetic_arranger(problems: list, show_results: bool = False):
 
-  32         1      9999      523
-+  8    - 3801    + 9999    -  49
-----    ------    ------    -----
-  40     -3800     19998      474
-'''
+    if check_problems(problems) == True:
+        first_line, second_line, third_line, results = "", "", "", ""
+
+        for operation in problems:
+            first, opera, second = operation.split()
+            result = calculate(first, second, opera)
+
+            diff = len(first) - len(second)
+            if diff > 0:
+                dashes = len(first) + 2
+                first_line += f"  {first}    "
+                second_line += f"{opera} {diff * ' '}{second}    "
+                third_line += f"{dashes * '-'}    "
+                results += f"{(dashes - len(str(result)))*' '}{result}    "
+                
+            elif diff < 0:
+                dashes = len(second) + 2
+                first_line += f"  {abs(diff) * ' '}{first}    "
+                second_line += f"{opera} {second}    "
+                third_line += f"{dashes * '-'}    "
+                results += f"{(dashes - len(str(result)))*' '}{result}    "
+
+            else:
+                dashes = len(first) + 2
+                first_line += f"  {first}    "
+                second_line += f"{opera} {second}    "
+                third_line += f"{dashes * '-'}    "
+                results += f"{(dashes - len(str(result)))*' '}{result}    "
+
+        first_line = first_line[:-4]
+        second_line = second_line[:-4]
+        third_line = third_line[:-4]
+        results = results[:-4]
+
+        if show_results == True:
+            return f"{first_line}\n{second_line}\n{third_line}\n{results}"
+
+        else:
+            solution = f"{first_line}\n{second_line}\n{third_line}"
+
+        return solution
+    else:
+        return check_problems(problems)
 
 
-def arithmetic_arranger(problems: list, second_argument: bool):
+def check_problems(liste: list):
+    if len(liste) > 5:
+        return ("Error: Too many problems.")
+    for operation in liste:
+        single = operation.split(" ")
+        first, second, third = single
+        if is_integer(first) and is_integer(third):
+            for item in single:
+                if second == "/" or second == "*":
+                    return ("Error: Operator must be '+' or '-'.")
+                elif len(item) > 4:
+                    return ("Error: Numbers cannot be more than four digits.")
+        else:
+            return "Error: Numbers must only contain digits."
+    else:
+        return True
 
-    first_line = []
-    second_line = []
-    third_line = []
-    results = []
 
-    for operation in problems:
-        first, opera, second = operation.split(" ")
-        
-        first_line.append(first)
-        second_line.append(opera + " " + second)
-
-        result = calculate(first, second, opera)
-        results.append(result)
-
-        maximum = max(len(first), len(second) + 2, len(str(result)))
-        third_line.append(maximum * "-")
-
-        
-    data = [first_line, second_line, third_line, results]
-    arranged_problems = ""
-    # print(data)
-    for i in range(4):
-        finall = f"{data[i][0]:>5}    {data[i][1]:>5}   {data[i][2]:>5}    {data[i][3]:>5}\n"
-        arranged_problems += finall
-        
-    return arranged_problems
-
-            
-
-def calculate(num1:int, num2:int, symbol:str):
+def calculate(num1: int, num2: int, symbol: str):
     if symbol == "+":
         return int(num1) + int(num2)
     elif symbol == "-":
         return int(num1) - int(num2)
-    elif symbol == "*":
-        return int(num1) * int(num2)
-    elif symbol == "/":
-        return int(num1) / int(num2)
-    
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+
+
+def is_integer(number):
+    try:
+        int(number)
+    except ValueError:
+        return False
+    else:
+        return True
--- boilerplate-arithmetic-formatter-1/main.py
+++ boilerplate-arithmetic-formatter-1/main.py
@@ -4,7 +4,7 @@ from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], second_argument=True))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
 # Run unit tests automatically
