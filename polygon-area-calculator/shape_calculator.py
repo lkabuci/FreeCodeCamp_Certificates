9046a642dce6915434b3153716f94f11c3a1e6f5
RedOne <elkaboussi@pm.me>
2022-04-18 01:01:24 +0000

    create rectangle class

--- polygon-area-calculator/shape_calculator.py
+++ polygon-area-calculator/shape_calculator.py
@@ -1,6 +1,40 @@
 class Rectangle:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def set_width(self):
+        pass
+
+    def set_height(self):
+        pass
+
+    def get_area(self):
+        '''Returns area (width * height)'''
+        pass
+
+    def get_perimeter(self):
+        '''Returns perimeter (2 * width + 2 * height)'''
+        pass
+
+    def get_diagonal(self):
+        '''Returns diagonal ((width ** 2 + height ** 2) ** .5)'''
+        pass
+
+    def get_picture(self):
+        '''Returns a string that represents the shape using lines of "*".'''
+        pass
+
+    def get_amount_inside(self):
+        pass
 
 
 
+class Square(Rectangle):
+    def __int__(self, side):
+        self.height = Rectangle.height
+        self.width = Rectangle.width
+        self.side = side
 
-class Square:
+    def set_side(self):
+        pass
